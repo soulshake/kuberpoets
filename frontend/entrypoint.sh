3ba3e133479c6fce461a7c3cd0cffb8dc6770773
AJ Bowen <aj@soulshake.net>
2022-08-26 12:51:35 +0200

    Commit frontend Dockerfile and ignorefiles

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Write some environment to ${DIST_DIR}/env.js at runtime.
+# The resulting file should be referenced in index.html.
+set -eux
+
+log() {
+    echo >&2 "[INFO] $*"
+}
+
+main() {
+    dist_dir="${DIST_DIR:-/usr/share/nginx/html}"
+    log "Generating ${dist_dir}/env.js..."
+
+    tee "${dist_dir}/env.js" >&2 << EOF
+window.env = {
+    ENV: "${ENV:-local}",
+    VERSION: "${VERSION:-unknown}",
+}
+console.log(window.env)
+EOF
+    log "Wrote to ${dist_dir}/env.js."
+
+    if [ -n "${*}" ]; then
+        log "Running provided command: $*"
+        exec "$@"
+    else
+        log "No command provided; assuming 'npm run start'"
+        exec npm run start
+    fi
+}
+
+main "$@"
