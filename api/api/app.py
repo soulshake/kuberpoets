4caabca839ce11e35c02280b905d3aae461fee8d
AJ Bowen <aj@soulshake.net>
2022-08-30 11:09:30 +0200

    Remove unused RequestFormatter

--- api/api/app.py
+++ api/api/app.py
@@ -6,25 +6,6 @@ from flask_cors import CORS
 from flask.logging import default_handler
 
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-
-        return super().format(record)
-
-
-formatter = RequestFormatter(
-    "[%(asctime)s] %(remote_addr)s requested %(url)s\n"
-    "%(levelname)s in %(module)s: %(message)s"
-)
-default_handler.setFormatter(formatter)
-
-
 dictConfig(
     {
         "version": 1,
